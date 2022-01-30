270322bc1fbe45b284bee820036d6f759ab4f004
thorntonmc <mt@mthornton.dev>
2022-01-29 20:07:13 -0500

    check if brew is installed

--- scripts/install/macos.sh
+++ scripts/install/macos.sh
@@ -1,2 +1,17 @@
-echo installing for macos...
+#!/bin/bash
 
+check_brew() {
+    brew -v &>/dev/null
+
+    if [ ! $? == 0 ]; then
+        echo "brew must be installed"
+        exit1
+    fi
+}
+
+install() {
+    check_brew # brew must be installed
+    echo install complete
+}
+
+install
