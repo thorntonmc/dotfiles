9d0dd29619b6b44e75db108230a9cebc6392695e
thorntonmc <mthornton@wasabi.com>
2022-04-24 17:02:29 -0400

    default editor

--- .zsh/01-environment.zsh
+++ .zsh/01-environment.zsh
@@ -5,3 +5,5 @@ path+="/usr/local/protobuf/bin"
 path+="/usr/local/opt/ruby/bin:/usr/local/lib/ruby/gems/3.0.0/bin"
 path+="$HOME/.local/share/gem/ruby/3.0.0/bin:$PATH"
 export TERM=xterm-256color
+
+export EDITOR=vim
