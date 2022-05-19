26ba22313322e9952a97273e5ae175e7b5c46fd6
thorntonmc <mthornton@wasabi.com>
2022-05-19 12:55:55 -0400

    safety

--- .zsh/03-scripts.zsh
+++ .zsh/03-scripts.zsh
@@ -1,20 +1,20 @@
 op-login() {
-    if [ $(op account get 2>/dev/null) ]; then
+    if [ "$(op account get 2>/dev/null)" ]; then
         echo "you are already logged in."
         return
     fi
 
-    if [ ! -f $HOME/1pw ]; then
+    if [ ! -f "$HOME/1pw" ]; then
         echo "1pw login not found in $HOME/1pw"
     fi
 
-    eval $(cat $HOME/1pw | op signin | cat)
+    eval "$(op signin < "$HOME/1pw")"
     echo "logged in to op"
 }
 
 op-get-pw() {
-    [ -z $1 ] && echo no args provided && return
+    [ -z "$1" ] && echo no args provided && return
 
-    op item get $1 --format=json | jq '.fields[0].value, .fields[1].value'
+    op item get "$1" --format=json | jq '.fields[0].value, .fields[1].value'
     return
 }
