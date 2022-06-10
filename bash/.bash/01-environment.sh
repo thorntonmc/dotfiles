dfd367fb2d4490d66bf6079b32d8ccc51082b765
thorntonmc <mthornton@wasabi.com>
2022-06-10 17:41:29 -0400

    start making scripts POSIX compliant for possible reuse

--- bash/.bash/01-environment.sh
+++ bash/.bash/01-environment.sh
@@ -1,6 +1,8 @@
 source $HOME/.local/scripts/git-prompt.sh
 PS1='\[\e[0;1;32m\]\u\[\e[0;1;32m\]@\[\e[0;1;32m\]\h\[\e[0m\]:\[\e[0;34m\]\w \[\e[0;93m\]$(__git_ps1 "[%s]")\n\[\e[0;32m\]\$ \[\e[0m\]'
 
+source $HOME/.local/scripts/ssh-agent-check.sh
+
 export GPG_TTY=$(tty)
 if [ -f ~/.ssh/agent.env ] ; then
     . ~/.ssh/agent.env > /dev/null
