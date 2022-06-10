6ee45959f6d838a668143a7c89e143c20183e534
thorntonmc <mthornton@wasabi.com>
2022-06-10 17:42:24 -0400

    move to posix compliant script

--- bash/.bash/01-environment.sh
+++ bash/.bash/01-environment.sh
@@ -4,16 +4,3 @@ PS1='\[\e[0;1;32m\]\u\[\e[0;1;32m\]@\[\e[0;1;32m\]\h\[\e[0m\]:\[\e[0;34m\]\w \[\
 source $HOME/.local/scripts/ssh-agent-check.sh
 
 export GPG_TTY=$(tty)
-if [ -f ~/.ssh/agent.env ] ; then
-    . ~/.ssh/agent.env > /dev/null
-    if ! kill -0 $SSH_AGENT_PID > /dev/null 2>&1; then
-        echo "Stale agent file found. Spawning a new agent. "
-        eval `ssh-agent | tee ~/.ssh/agent.env`
-        ssh-add
-    fi
-else
-    echo "Starting ssh-agent"
-    eval `ssh-agent | tee ~/.ssh/agent.env`
-    ssh-add
-fi
-
