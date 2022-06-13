1bf8f9403b1d8c68db1cfec8e2ad834c81d13ceb
thorntonmc <mthornton@wasabi.com>
2022-06-13 07:18:36 -0400

    deprecate ssh-agent for 1pw

--- bash/.bash/01-environment.sh
+++ bash/.bash/01-environment.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 export GPG_TTY
+export SSH_AUTH_SOCK
 
-source "${HOME}/.local/scripts/git-prompt.sh"
-source "${HOME}/.local/scripts/ssh-agent-check.sh"
+SSH_AUTH_SOCK=~/.1password/agent.sock
 
 PS1='\[\e[0;1;32m\]\u\[\e[0;1;32m\]@\[\e[0;1;32m\]\h\[\e[0m\]:\[\e[0;34m\]\w \[\e[0;93m\]$(__git_ps1 "[%s]")\n\[\e[0;32m\]\$ \[\e[0m\]'
 GPG_TTY=$(tty)
