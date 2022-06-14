1bc903d576c522ce04405c6f5bc15a54882f9768
thorntonmc <mthornton@wasabi.com>
2022-06-14 16:27:32 -0400

    convert to bash because i'm in charge

--- scripts/install-ansible.sh
+++ scripts/install-ansible.sh
@@ -1,4 +1,8 @@
-#!/bin/sh
+#!/bin/bash
+#
+# Determine package manager, and install ansible
+
+set -eo pipefail
 
 apt_install() {
     export DEBIAN_FRONTEND=noninteractive # skip tzinfo prompts
@@ -18,20 +22,29 @@ yum_install() {
     yum -y install ansible
 }
 
+function main() {
+    pm_found=1
 
-set -e
-pm_found=1
+    command -v ansible-playbook > /dev/null \
+        && echo "ansible already installed" \
+        && exit 0
 
-command -v ansible-playbook > /dev/null && echo "ansible already installed" && exit 0
+    for pm in pacman apt dnf yum; do
+        ! command -v "${pm}" >/dev/null && continue
+        pm_found=0
+        eval "${pm}_install"
+        break
+    done
 
-for pm in pacman apt dnf yum; do
-    ! command -v "${pm}" >/dev/null &&  echo "${pm}" not installed && continue
-    pm_found=0
-    eval "${pm}_install"
-    break
-done
+    [[ "${pm_found}" != 0 ]] \
+        && echo "package manager not found" \
+        && exit 1
 
-[ "${pm_found}" != 0 ] && echo "package manager not found" && exit 1
+    command -v ansible-playbook
+    exit "${?}"
+}
 
-command -v ansible-playbook
-exit "${?}"
+main "$@"
+
+
+
