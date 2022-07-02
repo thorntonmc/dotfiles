5299660faf6a92cd233c99714be34720e7d1fd7f
thorntonmc <mt@mthornton.dev>
2022-07-02 18:59:40 +0300

    work aliases

--- .bash/02-alias.sh
+++ .bash/02-alias.sh
@@ -4,3 +4,9 @@ alias ls="ls --color"
 alias l="ls --color"
 alias reload='source ${HOME}/.bashrc'
 alias vim=nvim
+
+# source work-specific aliases we dont track in
+# source control for privacy
+if [[ -r "${HOME}/work/.aliases" ]]; then
+  source "${HOME}/work/.aliases"
+fi
