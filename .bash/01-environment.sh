9ab222cf5888deced5f0cce05fb06ef6dd892105
thorntonmc <mt@mthornton.dev>
2022-09-06 17:58:18 +0300

    remove 1pw

--- .bash/01-environment.sh
+++ .bash/01-environment.sh
@@ -7,8 +7,6 @@ export ALACRITTY_CONFIG
 export ALACRITTY_THEMES
 export KUBE_CONFIG_DIR="${HOME}/.kube"
 
-SSH_AUTH_SOCK=~/.1password/agent.sock
-
 PS1='\[\e[0;1;32m\]\u\[\e[0;1;32m\]@\[\e[0;1;32m\]\h\[\e[0m\]:\[\e[0;34m\]\w \[\e[0;93m\]$(__git_ps1 "[%s]")\n\[\e[0;32m\]\$ \[\e[0m\]'
 GPG_TTY=$(tty)
 
