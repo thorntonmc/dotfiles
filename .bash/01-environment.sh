1719fbfe8f647570983683b4262822969ba6f324
thorntonmc <mt@mthornton.dev>
2022-12-06 12:28:50 +0300

    export editor

--- .bash/01-environment.sh
+++ .bash/01-environment.sh
@@ -7,11 +7,14 @@ export ALACRITTY_CONFIG
 export ALACRITTY_THEMES
 export KUBE_CONFIG_DIR="${HOME}/.kube"
 
+export EDITOR=vim
+
 PS1='\[\e[0;1;32m\]\u\[\e[0;1;32m\]@\[\e[0;1;32m\]\h\[\e[0m\]:\[\e[0;34m\]\w \[\e[0;93m\]$(__git_ps1 "[%s]")\n\[\e[0;32m\]\$ \[\e[0m\]'
 GPG_TTY=$(tty)
 
 PATH="${PATH}:${HOME}/go/bin"
 PATH="${PATH}:${HOME}/.local/bin"
+PATH=${PATH}:/opt/strongdm/bin/
 
 ALACRITTY_BASEDIR="${HOME}/.config/alacritty"
 ALACRITTY_CONFIG="${ALACRITTY_BASEDIR}/alacritty.yml"
