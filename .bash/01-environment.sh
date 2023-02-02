688fe8f94d12325f2ce456a514b1e8dac5cb6525
thorntonmc <mt@mthornton.dev>
2023-02-02 10:32:39 +0300

    lint

--- .bash/01-environment.sh
+++ .bash/01-environment.sh
@@ -9,14 +9,14 @@ export KUBE_CONFIG_DIR="${HOME}/.kube"
 export EDITOR
 
 if command -v nvim >/dev/null; then
-    EDITOR=nvim
-    alias vim=nvim
+  EDITOR=nvim
+  alias vim=nvim
 elif command -v vim >/dev/null; then
-    EDITOR=vim
+  EDITOR=vim
 elif command -v nano >/dev/null; then
-    EDITOR=nano
+  EDITOR=nano
 else
-    echo "WARNING: \$EDITOR not set"
+  echo "WARNING: \$EDITOR not set"
 fi
 
 PS1='\[\e[0;1;32m\]\u\[\e[0;1;32m\]@\[\e[0;1;32m\]\h\[\e[0m\]:\[\e[0;34m\]\w \[\e[0;93m\]$(__git_ps1 "[%s]")\n\[\e[0;32m\]\$ \[\e[0m\]'
@@ -30,5 +30,5 @@ ALACRITTY_BASEDIR="${HOME}/.config/alacritty"
 ALACRITTY_CONFIG="${ALACRITTY_BASEDIR}/alacritty.yml"
 ALACRITTY_THEMES="${ALACRITTY_BASEDIR}/themes"
 
-eval "ssh-agent -s"
+eval $(ssh-agent -s )
 ssh-add
